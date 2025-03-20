e3508ce06186be39f273473772bfb11040de4e01
isiah-a <isiaha478@gmail.com>
2025-03-20 16:48:53 -0400

    created function in tree, and created a list and ran program

--- tree.py
+++ tree.py
@@ -1,26 +1,21 @@
 import os
 
-# x = open('new_file', 'w')
-# x.write("Hello \n")
-# x.close()
-# x = open('new_file', 'r')
-# for line in x:
-#     print(line)
-# x.close()
+cwd = os.getcwd()
 
+def list_dir(start, alist):
+    dircontents = os.listdir(os.path.realpath(start))
+    with open('new_file', 'a') as f:
+        f.write(str(os.path.realpath(start)) + "\n")
+        f.close()
+    for i in dircontents:
+        newpath = os.path.join(start, i)
+        if os.path.isfile(cwd + '/' + str(newpath)):
+            alist.append(newpath)
+            with open('new_file', 'a') as f:
+                f.write(str(i) + '\n')
+                f.close()
+        elif os.path.isdir(cwd + '/' + str(newpath)):
+            list_dir(newpath, alist)
 
-# def tree(dir_path, file):
-#     with open(file, 'w') as f:
-#         for root, directories, files in os.walk(dir_path):
-#             for file in files:
-#                 f.write()
-
-
-dir = str(input("Insert your file path: "))
-find_directory = os.walk(dir)
-print(find_directory)
-
-# if is_directory:
-#     print(f"The path '{dir}' is a directory.")
-# else:
-#     print(f"The path '{dir}' is not a directory or does not exist.")
+new_list = []
+list_dir("PythonFundamentals.Exercises.Part10", new_list)
